039d3b71085a6884559d7122f51fc1c0f7e2a883
caojialei <caojialei@yonyou.com>
2018-03-30 16:28:54 +0800

    python学习测试

---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2018/3/30 16:28
+# @Author   : cjl
+# @Name     : test.py.py
+name = input("Please input your name: ")
+print("hello {0}", format(name))
+for i in range(1, 100):
+	print(i)
